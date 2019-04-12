9ed5e57168e7ceb885cdc39bd4574dfc044765a5
james.yu <james.yu@gogoro.com>
2019-04-12 23:29:11 +0800

    init

---
+++ build/precache-manifest.b1922937f4f7abe2c34961564d96dc2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "128ac43eaede03b7659b",
+    "url": "/static/js/main.269b0ab8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "128ac43eaede03b7659b",
+    "url": "/static/css/main.80b831ec.chunk.css"
+  },
+  {
+    "revision": "6adeb0601155aaf4839937043fd89408",
+    "url": "/index.html"
+  }
+];
